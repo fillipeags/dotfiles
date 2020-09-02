16694b7b15b9c2a79f4579a7476195f73691fdb0
Fillipe Augusto <38387593+fillipeags@users.noreply.github.com>
2020-09-01 22:19:00 -0300

    Create .hyper.js

---
+++ .hyper.js
@@ -0,0 +1,15 @@
+module.exports = {
+  config: {
+    updateChannel: 'canary',
+    shell: '/bin/zsh',
+    bell: 'SOUND',
+    macOptionSelectionMode: 'vertical',
+    webGLRenderer: true,
+    windowSize: [720, 520],
+    fontSize: 16,
+    fontFamily: 'Fira Code, Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+    lineHeight: 1,
+    letterSpacing: 0,
+  },
+  plugins: ['hyper-dracula', "hyperpower"]
+};
