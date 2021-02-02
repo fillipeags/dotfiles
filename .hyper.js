4739437c2ecc6ece933589899ecb9e55b461b64c
Fillipe Augusto <38387593+fillipeags@users.noreply.github.com>
2021-02-02 17:38:32 -0300

    update hyper.js

--- .hyper.js
+++ .hyper.js
@@ -5,11 +5,12 @@ module.exports = {
     bell: 'SOUND',
     macOptionSelectionMode: 'vertical',
     webGLRenderer: true,
-    windowSize: [720, 520],
+    windowSize: [920, 520],
     fontSize: 16,
     fontFamily: 'Fira Code, Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
     lineHeight: 1,
     letterSpacing: 0,
   },
+  
   plugins: ['hyper-dracula', "hyperpower"]
 };
